df40b2f4937fe1ef4045aa5eb11462067654d70f
shahnaazr <shahnaazr@users.noreply.github.com>
2020-03-06 16:03:28 +0000

    add jenkins file

---
+++ src/test/java/Tests.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Tests {
+
+    @Test
+    public void test(){
+        assertTrue(true);
+    }
+}
